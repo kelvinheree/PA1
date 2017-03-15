75eee46d72cfc01e86976745e0852381a652b51c
Kelvin <kelvinmei1996@gmail.com>
2017-03-14 20:12:39 -0400

    pushed signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
 
   validates :name,  presence: true, length: {maximum: 50 },
   uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[brandeis]+\.[edu]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get  '/help',    to: 'static_pages#help'
+  get  '/home',   to: 'static_pages#home'
+  get  '/signup',  to: 'users#new'
+  resources :users
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
